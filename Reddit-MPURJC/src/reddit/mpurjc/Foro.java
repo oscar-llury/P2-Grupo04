3861d70dcd7e1d4dbc87603bcdf977388aca651b
oscar <oscar.rivas.99@gmail.com>
2020-04-02 21:03:52 +0200

    create java project

---
+++ Reddit-MPURJC/src/reddit/mpurjc/Foro.java
@@ -0,0 +1,17 @@
+package reddit.mpurjc;
+
+/** Autores:
+ * Javier Espín Prieto
+ * Jorge Utrero Sevillano
+ * Miguel Cendrero Ortega
+ * Óscar Rivas Melar
+ */
+public class Foro {
+
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a Reddit URJC 2020");
+        Sistema s= new Sistema();
+        s.iniciarForo();
+    }
+    
+}
