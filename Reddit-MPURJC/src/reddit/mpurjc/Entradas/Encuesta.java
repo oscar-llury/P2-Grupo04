f8f4cb35b5a0b3cede12b3ce20873b53ba203f29
JSPINDEV <javier.espin@hotmail.es>
2020-04-04 21:22:30 +0200

    Creo encuesta con constructores

---
+++ Reddit-MPURJC/src/reddit/mpurjc/Entradas/Encuesta.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reddit.mpurjc.Entradas;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import reddit.mpurjc.Usuario;
+
+/**
+ *
+ * @author javie
+ */
+public class Encuesta implements TipoEntrada {
+    
+    
+    
+    public Encuesta(){}
+    
+    
+    public Encuesta(Usuario Usuario){
+    
+    
+    
+    }
+    
+    
+    
+
+    @Override
+    public boolean validar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void mostrar() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
