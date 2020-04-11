a64b2e8441858af69f1a07e968b35592d494bafb
JSPINDEV <javier.espin@hotmail.es>
2020-04-11 18:53:21 +0200

    Create command NuevaSubscripcion

---
+++ Reddit-MPURJC/src/reddit/mpurjc/ComandosSistema/NuevaSubscripcion.java
@@ -0,0 +1,46 @@
+
+package reddit.mpurjc.ComandosSistema;
+
+import reddit.mpurjc.Foro;
+import reddit.mpurjc.SubForo;
+import reddit.mpurjc.Usuario;
+
+/**
+ *
+ * @author javie
+ */
+public class NuevaSubscripcion extends ComandosSistema {
+
+    private SubForo subforo;
+    private Foro foro;
+    private Usuario usuarioActual;
+    
+    
+    public void NuevsSuscripcion(SubForo subforo){
+        this.subforo=subforo;
+    }
+    @Override
+    public boolean ejecutar(String s) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+   
+        if(!foro.contieneSubForo(s)){
+            SubForo nuevoSubForo = new SubForo(s);
+            usuarioActual.addSubscripcion(nuevoSubForo);
+            return true;
+        }else{
+        return false;
+        }
+    }
+
+    @Override
+    public boolean comprobar(String s) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setForo(Foro foro) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
