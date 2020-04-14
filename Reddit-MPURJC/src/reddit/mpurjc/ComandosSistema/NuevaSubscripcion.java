efd114b71f7b5cb5b9e763e6ca2249af46df67e1
JSPINDEV <javier.espin@hotmail.es>
2020-04-14 15:50:56 +0200

    Update NuevaSubscripcion

--- Reddit-MPURJC/src/reddit/mpurjc/ComandosSistema/NuevaSubscripcion.java
+++ Reddit-MPURJC/src/reddit/mpurjc/ComandosSistema/NuevaSubscripcion.java
@@ -1,6 +1,7 @@
 
 package reddit.mpurjc.ComandosSistema;
 
+import reddit.mpurjc.Entradas.Entrada;
 import reddit.mpurjc.Foro;
 import reddit.mpurjc.SubForo;
 import reddit.mpurjc.Usuario;
@@ -11,25 +12,34 @@ import reddit.mpurjc.Usuario;
  */
 public class NuevaSubscripcion extends ComandosSistema {
 
-    private SubForo subforo;
+    private SubForo subforoActual;
     private Foro foro;
     private Usuario usuarioActual;
+    private Entrada entradaActual;
     
     
-    public void NuevsSuscripcion(SubForo subforo){
-        this.subforo=subforo;
+    public void NuevsSuscripcion(Foro foro){
+       this.foro = foro;
+       this.subforoActual=foro.getSubForoActual();  
+       this.usuarioActual = foro.getUsuarioActual();
+       this.entradaActual = foro.getEntradaActual();  
     }
+    
+    
     @Override
     public boolean ejecutar(String s) {
-        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       
    
         if(!foro.contieneSubForo(s)){
            
-           subforo=foro.getSubForo(s);
-           usuarioActual.addSubscripcion(subforo);
+            subforoActual=foro.getSubForo(s);
+            
+            usuarioActual.addSubscripcion(subforoActual);
            
             return true;
+            
         }else{
+            System.out.println("El foro no contiene ese subforo");
             return false;
         }
     }
@@ -37,11 +47,18 @@ public class NuevaSubscripcion extends ComandosSistema {
     @Override
     public boolean comprobar(String s) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        
     }
 
     @Override
     public void setForo(Foro foro) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        this.foro=foro;
+        this.usuarioActual=foro.getUsuarioActual();
+        this.subforoActual=foro.getSubForoActual();
+        this.entradaActual=foro.getEntradaActual();
     }
     
     
