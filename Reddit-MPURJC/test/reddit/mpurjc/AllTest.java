72352d5d8623468f5a8fc0400d8899d4087adc06
oscar <oscar.rivas.99@gmail.com>
2020-05-06 19:59:30 +0200

    added library junit

---
+++ Reddit-MPURJC/test/reddit/mpurjc/AllTest.java
@@ -0,0 +1,19 @@
+package reddit.mpurjc;
+
+/**
+ * Autores Práctica MP: Javier Espín Prieto Jorge Utrero Sevillano Miguel
+ * Cendrero Ortega Óscar Rivas Melar
+ */
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    
+})        
+        
+public class AllTest {
+    
+}
