0e7626670d4861b70cb1f8a58049f9aece25f765
JSPINDEV <javier.espin@hotmail.es>
2020-05-11 13:24:36 +0200

    Test NuevaSubscripcion done and modify NuevaSusbcripcion

--- Reddit-MPURJC/test/reddit/mpurjc/ComandosSistema/NuevaSubscripcionTest.java
+++ Reddit-MPURJC/test/reddit/mpurjc/ComandosSistema/NuevaSubscripcionTest.java
@@ -12,18 +12,25 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 import reddit.mpurjc.Foro;
+import reddit.mpurjc.SubForo;
+import reddit.mpurjc.Usuario;
 
-/**
- *
- * @author javie
- */
+
 public class NuevaSubscripcionTest {
+    
+     private static final Foro reddit = new Foro();
     
     public NuevaSubscripcionTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
+        
+        NuevoUsuario comandoNuevoUsuario = new NuevoUsuario(reddit);
+        NuevoSubForo comandoNuevoSubForo = new NuevoSubForo(reddit);
+        
+        comandoNuevoUsuario.ejecutar("NuevoUsuario(Profesor1,ApellidosUsuario,emailProfesor@urjc.es,pass)");
+        comandoNuevoSubForo.ejecutar("NuevoSubForo(SubForo 1)");
     }
     
     @AfterClass
@@ -41,44 +48,82 @@ public class NuevaSubscripcionTest {
     /**
      * Test of ejecutar method, of class NuevaSubscripcion.
      */
-    @Test
-    public void testEjecutar() {
-        System.out.println("ejecutar");
-        String s = "";
-        NuevaSubscripcion instance = null;
-        boolean expResult = false;
-        boolean result = instance.ejecutar(s);
+     @Test
+    public void testEjecutar_Repetido() {
+        System.out.println("***Test NuevaSubscricion Repetida***");
+        
+        Login comandoLogin = new Login(reddit);
+        comandoLogin.ejecutar("Login(emailProfesor pass)");
+      
+       
+        NuevaSubscripcion instance = new NuevaSubscripcion(reddit);
+        instance.ejecutar("NuevaSubscripcion(SubForo 1)"); //lo ejecutamos para que ya este susbcrito
+        
+        boolean result = instance.ejecutar("NuevaSubscripcion(SubForo 1)");
+        boolean expResult=false;
+        
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of comprobar method, of class NuevaSubscripcion.
-     */
+    }
+    
+   
+     
     @Test
-    public void testComprobar() {
-        System.out.println("comprobar");
-        String s = "";
-        NuevaSubscripcion instance = null;
+    public void testEjecutar_SinUsuarioLogeado() {
+        System.out.println("****Test NuevaSusbcripcion Sin Usuario Logeado****");
+        
+        Logout comandoLogout = new Logout(reddit);
+        comandoLogout.ejecutar("Logout()");
+        
+        NuevaSubscripcion instance = new NuevaSubscripcion(reddit);
+        
+        boolean result = instance.ejecutar("NuevaSubscripcion(SubForo 1)");
+        
         boolean expResult = false;
-        boolean result = instance.comprobar(s);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
 
-    /**
-     * Test of setForo method, of class NuevaSubscripcion.
-     */
+        assertEquals(expResult, result);
+
+    }
+    
     @Test
-    public void testSetForo() {
-        System.out.println("setForo");
-        Foro foro = null;
-        NuevaSubscripcion instance = null;
-        instance.setForo(foro);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+    public void testEjecutar_SubforoNoExiste() {
+        System.out.println("*******Test NuevaSubscripcion Subforo NO existe*******");
+        
+        Login comandoLogin = new Login(reddit);
+        comandoLogin.ejecutar("Login(emailProfesor pass)");
+        
+        NuevaSubscripcion instance = new NuevaSubscripcion(reddit);
+        
+        boolean result = instance.ejecutar("NuevaSubscripcion(SubForo 2)");
+        
+        boolean expResult = false;
+
+        assertEquals(expResult, result);
+        
+        
+    
+    }
+    
+    @Test
+    public void testEjecutar_Correcto() {
+        System.out.println("*******Test NuevaSubscripcion CORRECTAMENTE*******");
+        
+        
+        Login comandoLogin = new Login(reddit);
+        comandoLogin.ejecutar("Login(emailProfesor pass)");
+        
+        
+        NuevoSubForo comandoNuevoSubForo = new NuevoSubForo(reddit);
+        comandoNuevoSubForo.ejecutar("NuevoSubForo(SubForo 2)");
+        
+       NuevaSubscripcion instance = new NuevaSubscripcion(reddit);
+        
+        boolean result = instance.ejecutar("NuevaSubscripcion(SubForo 2)");
+        
+        boolean expResult = true;
+
+        assertEquals(expResult, result);
+
     }
     
 }
